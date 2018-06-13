a10c1231ff8f9915b65a358889620558933954ac
desperado321 <staffa261@gmail.com>
2018-06-13 12:40:59 +0300

    Lesson 3 correct

--- Classes.rb
+++ Classes.rb
@@ -1,27 +1,26 @@
 class Station
-    attr_reader :trains,:name
+    attr_reader :trains, :name
 
-    def initialize (name="First")
+    def initialize(name)
       @name=name
       @trains=[]
     end
 
     def get_train(train)  #принимаем поезд
-      if @trains.include? (train)
+      if @trains.include?(train)
         return
       end
       @trains << train
     end
   
-    def send_train (train)
-      if @trains.include? (train)
+    def send_train(train)
+      if @trains.include?(train)
         return
       end
-      # @trains.pop вопрос для наставника, просто сначала я хотел удалять последний элемент массива в задании, можно так?
-      @trains.delete (train)
+      @trains.delete(train)
     end
   
-    def get_train_type (type)
+    def get_train_type(type)
       trains.select {|train| train.type == type}
     end
   end
@@ -29,39 +28,35 @@ class Station
 class Train
     attr_reader :currspeed, :count_vag
   
-    def initialize (number,type,count_vag)
+    def initialize(number,type,count_vag)
       @number=number
       @type=type
       @count_vag=count_vag
       @currspeed=0
-      @currPosition = 0 #индекс в массиве для перемещения по массиву станций
+      @curr_position = 0 #индекс в массиве для перемещения по массиву станций
     end
   
     def add_vag #цепляем вагон
       if @currspeed == 0
          @count_vag += 1
-      else
-         return
       end
     end
 
     def rem_vag #удаляем вагон
       if @currspeed == 0
          @count_vag += 1
-      else
-         return
       end
     end
 
-    def up_speed (speed) #набираем скорость
+    def up_speed(speed) #набираем скорость
       @currspeed += speed
     end
 
-    def stop () #тормозим
+    def stop  #тормозим
       @currspeed = 0
     end
 
-    def get_route (route) #назначим марщрут поезду и сразу поместим его на первую станцию в маршруте
+    def get_route(route) #назначим марщрут поезду и сразу поместим его на первую станцию в маршруте
       @route = route
       @route.station.first.get_train(self) #поместим поезд на станцию
     end
@@ -107,19 +102,17 @@ end
 class Route
     attr_reader :stations
 
-    def initialize (first_station,last_station)
+    def initialize(first_station,last_station)
       @stations = [first_station,last_station]
     end
 
-    def add_station (station) #Добавим промежуточную станцию
+    def add_station(station) #Добавим промежуточную станцию
       @stations.insert((@stations.size-1),station)
     end
   
-    def del_station (station) #удаляем промежуточную станцию
+    def del_station(station) #удаляем промежуточную станцию
       if (station != @stations.first) || (station != @stations.last)
         @stations.delete (station)
-      else
-        return
       end
     end
 
@@ -127,6 +120,7 @@ class Route
       @stations.each {|n| puts n}
     end
 end
+
   
   
   
